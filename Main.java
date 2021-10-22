afe9be15fabe372e1d2289369f5ab17638afb8b9
olegvasylovych <91026120+olegvasylovych@users.noreply.github.com>
2021-10-23 00:27:49 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,13 +7,13 @@ public class Main {
     public static void main(String[] args) {
         int arr[] = { 1212, 544, 34, 6654, 454, 32, 54, 54 };
 
-        System.out.println("Початковий масив");
+        System.out.println("initial array");
         printArray(arr);
 
         MergeSort ob = new MergeSort();
         ob.sort(arr, 0, arr.length - 1);
 
-        System.out.println("\nВідсортований масив");
+        System.out.println("\n sorted array");
         printArray(arr);
     }
 }
